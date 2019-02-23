9943cb0388c67d03576d56e0ca2ba5aeaa0370a1
tomoki <tomokiakiyama@docomo.ne.jp>
2019-02-23 17:35:48 +0900

    chap3

---
+++ chap3/challenge/challenge3_5.py
@@ -0,0 +1,11 @@
+#変数ageに整数を代入して、そのageを使って何かしらの条件分岐を使って条件におうじてメッセージを出力しよう
+
+age =48
+if age <= 19:
+    print("少年")
+elif age <= 39:
+    print("青年")
+elif age <= 59:
+    print("壮年")
+else:
+    print("老年")
---
+++ chap3/ex/if_sample1.py
@@ -0,0 +1,5 @@
+home = "America" 
+if home == "America": 
+     print("Hello, America!") 
+else: 
+    print("Hello, World!") 
