beff911cb7fea38546afebecbe348afd987b6b89
tomoki <tomokiakiyama@docomo.ne.jp>
2019-03-07 17:57:20 +0900

    challenge

---
+++ chap5_containers/challenge/challenge_1.py
@@ -0,0 +1,5 @@
+#好きなミュージシャンのリストを作ろう
+
+artists = ["aiko", "嵐", "中島美嘉", "back number"]
+
+print(artists)
---
+++ chap5_containers/challenge/challenge_5.py
@@ -0,0 +1,7 @@
+artists = {"aiko": ["カブトムシ", "花火", "milk"],
+           "嵐": ["ARASHI", "Happiness", "Monstar"],
+           "中島美嘉": ["雪の華", "LIFE", "Glamorous sky"],
+           "back number": ["ハッピーエンド", "クリスマスソング", "高嶺の花子さん"]
+    }
+
+print(artists)
